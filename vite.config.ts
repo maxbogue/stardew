60800a891ec8475cdf6cf6f306c9c1db33136d2f
Max Bogue <max.bogue@pathai.com>
2024-06-30 15:22:57 -0400

    Deploy to GitHub Pages.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
   ],
@@ -12,6 +13,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build:{
+    outDir: 'docs',
   },
   define: {
     VERSION: JSON.stringify(process.env.npm_package_version),
